8c8f4c7385d667134fb02416294c1f6dee80ff11
Anubhav Gupta <anubhav.gupta@gmail.com>
2025-11-16 21:23:16 +0000

    pushing some updates

--- server.js
+++ server.js
@@ -8,9 +8,12 @@ app.use(cors());
 app.use(express.json());
 
 // 🔗 connect to database
-mongoose.connect("mongodb+srv://anubgupt_db_user:fNrpuizDWNPjeQPR@myfirstapp-mongodb.actj3nf.mongodb.net/?appName=myfirstapp-mongodb")
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+  .catch(err => console.error("MongoDB connection error:", err));
 
 // 🧾 Save data route
 app.post("/save", async (req, res) => {
